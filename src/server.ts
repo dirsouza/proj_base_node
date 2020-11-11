6b138643c336c6362dc330986708018e21cd74fc
Diogo Souza <diogo.souza@uds.com.br>
2020-11-10 23:58:29 -0400

    add eslint

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-    return res.json({ message: 'Hello world' });
-});
+app.get('/', (req: Request, res: Response) => res.json({ message: 'Hello world' }));
 
 app.listen(3333);
